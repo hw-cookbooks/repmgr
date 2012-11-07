4bab172998610fb22567065aa41192ffe6cd6577
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-07 12:30:25 -0800

    Isolate installation to explicit recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,40 +1,5 @@
-include_recipe 'build-essential'
-
-(node[:repmgr][:packages][:dependencies] + Array(node[:repmgr][:packages][:pg_dev])).each do |pkg|
-  package pkg
-end
-
-r_url = File.join(node[:repmgr][:base_uri], "repmgr-#{node[:repmgr][:version]}.tar.gz")
-r_local = File.join(node[:repmgr][:build_dir], File.basename(r_url))
-pg_bin = %x{pg_config --bindir}.strip
-
-directory node[:repmgr][:build_dir]
-
-remote_file r_local do
-  source r_url
-  action :create_if_missing
-end
-
-execute "unpack #{File.basename(r_local)}" do
-  command "tar xzf #{r_local}"
-  cwd File.dirname(r_local)
-  creates r_local.sub('.tar.gz', '')
-end
-
-execute "configure repmgr v#{node[:repmgr][:version]}" do
-  command "make USE_PGXS=1 install"
-  cwd File.dirname(r_local)
-  creates File.join(pg_bindir, 'repmgr')
-end
-
-template '/etc/init.d/repmgrd' do
-  source 'repmgrd.init.erb'
-  variables(
-    :bin_path => File.join(pg_bindir, 'repmgr'),
-    :el => node.platform_family == 'rhel'
-
-  )
-end
+include_recipe 'repmgr::install'
+include_recipe 'repmgr::configure'
 
 service 'repmgrd' do
   action [:enable, :start]
