8276c6c7763d83dd21213b9943d55b66417e158d
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-08 08:20:18 -0800

    Include setup recipe by default

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 include_recipe 'postgresql::replication'
 include_recipe 'repmgr::install'
 include_recipe 'repmgr::configure'
+include_recipe 'repmgr::setup'
 
 service 'repmgrd' do
   action [:enable, :start]
