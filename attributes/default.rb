2ad4ba623dcb2d565e335820a1ead5c52c974592
Darrin Eden <darrin.eden@gmail.com>
2012-12-11 18:41:27 -0800

    libpq-dev is already installed.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default[:repmgr][:version] = '1.2.0'
 default[:repmgr][:build_dir] = '/var/cache/repmgr'
 # TODO: RHEL based packages
 default[:repmgr][:packages][:pg_dev] = 'postgresql-server-dev-9.1'
-default[:repmgr][:packages][:dependencies] = %w(libxslt1-dev libpam0g-dev libedit-dev libpq-dev)
+default[:repmgr][:packages][:dependencies] = %w(libxslt1-dev libpam0g-dev libedit-dev)
 default[:repmgr][:repmgrd_bin] = '/usr/bin/repmgrd'
 default[:repmgr][:repmgr_bin] = '/usr/bin/repmgr'
 default[:repmgr][:config_file_path] = '/etc/repmgr/repmgr.conf'
