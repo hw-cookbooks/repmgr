e966d1d81b1143639d87907c77b0885eae0d8c73
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-31 08:25:25 -0800

    Add info and set version 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,12 @@
-version '0.0.1'
+name 'repmgr'
+maintainer 'Heavy Water'
+maintainer_email 'support@hw-ops.com'
+license 'Apache 2.0'
+description 'Replication manager for PostgreSQL'
 
+version '0.1.0'
+
+depends 'postgresql', '>= 2.0.0'
 depends 'build-essential'
 depends 'database'
 depends 'discovery'
