7e528d18955b67a5c79ac764f9ca7c0853c924d4
shaundomingo-ninefold <shaun@ninefold.com>
2013-03-19 17:51:15 +1100

    Add log level.

--- attributes/default.rb
+++ attributes/default.rb
@@ -40,3 +40,4 @@ default[:repmgr][:replication][:database] = 'replication_db'
 default[:repmgr][:replication][:standby_feedback] = true
 default[:repmgr][:replication][:hostname] = 'localhost'
 default[:repmgr][:replication][:common_environment] = true
+default[:repmgr][:log_level] = 'debug'
