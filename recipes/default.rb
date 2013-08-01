9e8829579c61f432141619b62017d0c6c0223d3e
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-01 16:06:58 -0700

    Include addressing helper by default

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,7 @@ include_recipe 'repmgr::composite_attributes'
 
 package 'rsync'
 
+include_recipe 'repmgr::addressing'
 include_recipe 'repmgr::install'
 include_recipe 'repmgr::configure'
 include_recipe 'repmgr::setup'
