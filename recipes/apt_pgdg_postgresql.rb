b95ac274e3027fcc0709a32e099bc45b450bec2e
Sean Porter <portertech@gmail.com>
2013-10-30 13:04:03 -0700

    use apt preference to fix the postgres package depencency versions

--- recipes/apt_pgdg_postgresql.rb
+++ recipes/apt_pgdg_postgresql.rb
@@ -2,4 +2,14 @@ include_recipe 'postgresql::apt_pgdg_postgresql'
 
 resources('apt_repository[apt.postgresql.org]').components ['main', node['postgresql']['version']]
 
-include_recipe 'package_installer'
+apt_preference 'pgdg_pin_1' do
+  package_name '*'
+  pin "release c=#{node['postgresql']['version']}"
+  pin_priority '1000'
+end
+
+apt_preference 'pgdg_pin_2' do
+  package_name '*'
+  pin "release n=#{node['lsb']['codename']}-pgdg, v=#{node['postgresql']['version']}"
+  pin_priority '600'
+end
