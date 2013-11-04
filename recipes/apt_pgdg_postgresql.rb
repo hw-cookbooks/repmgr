906b1d54c1a86599ca864fb2b4e8dc66ab666bf0
Sean Porter <portertech@gmail.com>
2013-11-04 13:37:08 -0800

    setup postgresql apt repo and pins during compile time

--- recipes/apt_pgdg_postgresql.rb
+++ recipes/apt_pgdg_postgresql.rb
@@ -1,15 +1,19 @@
 include_recipe 'postgresql::apt_pgdg_postgresql'
 
-resources('apt_repository[apt.postgresql.org]').components ['main', node['postgresql']['version']]
+postgresql_apt = resources('apt_repository[apt.postgresql.org]')
+postgresql_apt.components ['main', node['postgresql']['version']]
+postgresql_apt.run_action(:add)
 
-apt_preference 'pgdg_pin_1' do
+pin1 = apt_preference 'pgdg_pin_1' do
   package_name '*'
   pin "release c=#{node['postgresql']['version']}"
   pin_priority '1000'
 end
+pin1.run_action(:add)
 
-apt_preference 'pgdg_pin_2' do
+pin2 = apt_preference 'pgdg_pin_2' do
   package_name '*'
   pin "release n=#{node['lsb']['codename']}-pgdg, v=#{node['postgresql']['version']}"
   pin_priority '600'
 end
+pin2.run_action(:add)
