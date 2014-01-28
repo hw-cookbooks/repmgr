03be4f141b477b3d50ce29ee341e65cfb65db685
Warren Bain <warren@ninefold.com>
2014-01-28 15:35:58 +1100

    Ensure race condition doesn't save invalid id no.
    * the dumb_repmgr_id search looks for the highst id number in use
      but if multiple nodes are coming up, they will get the same
      number. The search saves this id and in case of a conflict it
      will never update and the standby clone cmd will fail every time
    
    * using the default attribute will allow another search to occur
      in the event of a conflict and the new id will then work

--- recipes/dumb_repmgr_id.rb
+++ recipes/dumb_repmgr_id.rb
@@ -7,7 +7,9 @@ unless(node[:repmgr][:config][:node])
     :empty_ok => true
   )
 
-  node.set[:repmgr][:config][:node] = Array(nodes).map{|r_n|
+  # don't save this now - race condition can occur when multiple
+  # standby nodes come up together, smart_id will save it later
+  node.default[:repmgr][:config][:node] = Array(nodes).map{|r_n|
     r_n[:repmgr][:config][:node].to_i
   }.max.to_i + 1
 end
