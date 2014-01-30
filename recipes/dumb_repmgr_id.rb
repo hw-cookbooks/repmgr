2944850b5576a4e397ef8e6bd00e0b909eadfb75
Warren Bain <warren@ninefold.com>
2014-01-30 13:49:57 +1100

    Correct issue when a new node replaces a pre-existing node and number is reused

--- recipes/dumb_repmgr_id.rb
+++ recipes/dumb_repmgr_id.rb
@@ -1,13 +1,26 @@
-unless(node[:repmgr][:node_status_confirmed])
-  nodes = discovery_all(
-    'repmgr_config_node:*', 
-    :raw_search => true,
-    :environment_aware => node[:repmgr][:replication][:common_environment],
-    :minimum_response_time_sec => false,
-    :empty_ok => true
-  )
+# race conditions can occur whereby nodes get conflicting
+# id numbers and repmgr registration will fail
+# * multiple nodes are started at the same time
+#   - both get the same starting number
+# * a node is added after a node has been deleted
+#   - it will get a number that has already been used
+# previous code relies on node attributes not being
+# saved until end of run but other recipes can do this
 
-  node.set[:repmgr][:config][:node] = Array(nodes).map{|r_n|
-    r_n[:repmgr][:config][:node].to_i
-  }.max.to_i + 1
+unless(node[:repmgr][:node_status_confirmed])
+  unless(node[:repmgr][:config][:node])
+    nodes = discovery_all(
+      'repmgr_config_node:*', 
+      :raw_search => true,
+      :environment_aware => node[:repmgr][:replication][:common_environment],
+      :minimum_response_time_sec => false,
+      :empty_ok => true
+    )
+
+    node.set[:repmgr][:config][:node] = Array(nodes).map{|r_n|
+      r_n[:repmgr][:config][:node].to_i
+    }.max.to_i + 1
+  else
+    node.set[:repmgr][:config][:node] = node[:repmgr][:config][:node].to_i + 1
+  end
 end
