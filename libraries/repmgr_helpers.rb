d5ae11a56615f42e0af2461ad6cda9158e126945
Warren Bain <warren@ninefold.com>
2014-05-22 15:09:37 +1000

    Refactor helper code from recipes into library

--- libraries/repmgr_helpers.rb
+++ libraries/repmgr_helpers.rb
@@ -16,11 +16,46 @@ module RepmgrHelpers
     end
   end
 
+  def postgresql_in_consistent_state?
+    postgresql_log_file = "/var/log/postgresql/postgresql-#{node[:postgresql][:version]}-main.log"
+    consistent_state_reached = false
+    file_last_lines(postgresql_log_file, 1000).reverse_each do |line|
+      if line =~ /database system is ready to accept read only connections/
+        consistent_state_reached = true
+        break
+      elsif line =~ /entering standby mode/
+        break
+      end
+    end
+    consistent_state_reached
+  end
+
   def file_last_lines(file, lines = 100)
     # no need to read an entire file and
     # no need for complicated IO#seek on 'nix
     %x(tail -n #{lines} '#{file}').split(/\r?\n/)
   end
+
+  def find_master_node
+    if node[:repmgr][:discovery][:master_role]
+      search_term = node[:repmgr][:discovery][:master_role]
+      raw_search = false
+    else
+      search_term = 'replication_role:master'
+      raw_search = true
+    end
+
+    with_retries do
+      discovery_search(
+        search_term,
+        :raw_search => raw_search,
+        :environment_aware => node[:repmgr][:replication][:common_environment],
+        :minimum_response_time_sec => false,
+        :empty_ok => false
+      )
+    end
+  end
+
 end
 
 Chef::Recipe.send(:include, RepmgrHelpers)
